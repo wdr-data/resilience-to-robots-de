ce3934499cf1999b51cfbc296ce9af7d77fb508e
Jannes Höke <mail@jhoeke.de>
2023-05-08 19:39:56 +0200

    🐛 Remove dependency on material icons

--- src/components/jobchecker/jobchecker.jsx
+++ src/components/jobchecker/jobchecker.jsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useState } from "react";
 import Autocomplete from "@mui/joy/Autocomplete";
-import IconSearch from "@mui/icons-material/Search";
 
 import dataOriginal from "../../../data/ai_occupations_deepl.json";
 import styles from "./jobchecker.module.css";
@@ -20,6 +19,23 @@ const riskNumberFormatter = new Intl.NumberFormat("de-DE", {
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
 });
+
+const IconSearch = () => (
+  <svg
+    style={{
+      display: "block",
+      width: "24px",
+      height: "24px",
+      fill: "currentColor",
+    }}
+    focusable="false"
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+    data-testid="SearchIcon"
+  >
+    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+  </svg>
+);
 
 const JobChecker = () => {
   const [job, setJob] = useState(null);
